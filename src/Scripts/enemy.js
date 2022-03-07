c23ada6f00adc7719cafc26c962465a103d0909b
Rangerdevv <meerabdul2008@gmail.com>
2022-03-06 21:19:22 -0500

    Finised the outside world and also added a new battle. some modification in the battle.js and the Index.html. Fixed typos and added currency to the game.

--- src/Scripts/enemy.js
+++ src/Scripts/enemy.js
@@ -5,15 +5,26 @@ const fire_minion = {
     attack: 20,
 }
 
+const shadow_figure = {
+    name: "Shadow Figure",
+    health: 40,
+    maxHealth: 40,
+    attack: 25,
+}
+
 // get the player
-import {player,textdialogue,choiceA,choiceB, playerattack, playerdefend , playerheal, Playerchoice, FightScene, Dialogue , enemyhealth, PrisonDialogue, FightDialogue, playerkick, playerplasma, PlayerStamina1 , PlayerStamina2} from './game.js';
+import {player,textdialogue,choiceA, playerattack, playerdefend, playerheal, Playerchoice, FightScene, Dialogue, enemyhealth, PrisonDialogue, FightDialogue, playerkick, playerplasma, PlayerStamina1 , PlayerStamina2, playerhealth2} from './game.js';
 
 export function fight() {
+    fire_minion.health = 30;
+    player.stamina = 100;
+    player.health = 100;
+    playerhealth2.innerText = "Health: " + player.health;
     FightScene.style.display = "block";
     Dialogue.style.display = "none";
     playerattack.InnerText = "Attack";
     PlayerStamina1.innerText = player.stamina;
-    PlayerStamina2.innerText = player.stamina;
+    PlayerStamina2.innerText = "Stamina: " + player.stamina;
     enemyhealth.innerText = "Health: "+fire_minion.health;
     playerheal.InnerText = "Heal";
     playerdefend.InnerText = "Defend";
@@ -24,7 +35,7 @@ export function fight() {
         enemyhealth.innerText = "Health: " + fire_minion.health;
         FightDialogue.innerText = "You kicked the " + fire_minion.name + "!";
         PlayerStamina1.innerText = player.stamina;
-        PlayerStamina2.innerText = player.stamina;
+        PlayerStamina2.innerText = "Stamina: " + player.stamina;
         // wait for 1 second and then call fire_minion_attack
         if (fire_minion.health >= 0) {
             setTimeout(fire_minion_attack, 1000);
@@ -36,7 +47,7 @@ export function fight() {
         enemyhealth.innerText = "Health: " + fire_minion.health;
         FightDialogue.innerText = "You have used plasma! -50 energy and 60 damage!";
         PlayerStamina1.innerText = player.stamina;
-        PlayerStamina2.innerText = player.stamina;
+        PlayerStamina2.innerText = "Stamina: " + player.stamina;
         // wait for 1 second and then call fire_minion_attack
         if (fire_minion.health >= 0) {
             setTimeout(fire_minion_attack, 1000);
@@ -48,7 +59,7 @@ export function fight() {
         enemyhealth.innerText = "Health: " + fire_minion.health;
         FightDialogue.innerText = "You attacked the " + fire_minion.name + " for " + player.attack + " damage!";
         PlayerStamina1.innerText = player.stamina;
-        PlayerStamina2.innerText = player.stamina;
+        PlayerStamina2.innerText = "Stamina: " + player.stamina;
         // wait for 1 second and then call fire_minion_attack
         if (fire_minion.health >= 0) {
             setTimeout(fire_minion_attack, 1000);
@@ -60,7 +71,7 @@ export function fight() {
         enemyhealth.innerText = "Health: " + fire_minion.health;
         FightDialogue.innerText = "You healed yourself for 15 health and 10 stamina!";
         PlayerStamina1.innerText = player.stamina;
-        PlayerStamina2.innerText = player.stamina;
+        PlayerStamina2.innerText = "Stamina: " + player.stamina;
         // wait for 1 second and then call fire_minion_attack
         if (fire_minion.health >= 0) {
         setTimeout(fire_minion_attack, 1000);
@@ -72,18 +83,18 @@ export function fight() {
         enemyhealth.innerText = "Health: " + fire_minion.health;
         FightDialogue.innerText = "You defended yourself!";
         PlayerStamina1.innerText = player.stamina;
-        PlayerStamina2.innerText = player.stamina;
+        PlayerStamina2.innerText = "Stamina: " + player.stamina;
         // wait for 1 second and then call fire_minion_attack
-        if (fire_minion.health >= 0) {
-            setTimeout(fire_minion_attack, 1000);
-            }
+        setTimeout(() => {
+            Playerchoice.style.display = "block";
+        }, 1000);
     }
     function playerattacks() {
         fire_minion.health -= player.attack;
         player.stamina -= 10;
     }
     function playerdefends() {
-        player.health -= fire_minion.attack / 2;
+        player.health -= fire_minion.attack / 5;
         player.stamina -= 5;
     }
     function playerheals() {
@@ -128,3 +139,135 @@ export function fight() {
         }
     }
 }
+
+// shadow guy
+
+export function shadowfigure() {
+    shadow_figure.health = 40;
+    playerhealth2.innerText = "Health: " + player.health;
+    FightScene.style.display = "block";
+    Dialogue.style.display = "none";
+    playerattack.InnerText = "Attack";
+    PlayerStamina1.innerText = player.stamina;
+    PlayerStamina2.innerText = "Stamina: " + player.stamina;
+    enemyhealth.innerText = "Health: "+shadow_figure.health;
+    playerheal.InnerText = "Heal";
+    playerdefend.InnerText = "Defend";
+    textdialogue.InnerText = "You have encountered a " + shadow_figure.name + "!";
+    playerkick.onclick = function() {
+        playerkicks();
+        Playerchoice.style.display = "none";
+        enemyhealth.innerText = "Health: " + shadow_figure.health;
+        FightDialogue.innerText = "You kicked the " + shadow_figure.name + "!";
+        PlayerStamina1.innerText = player.stamina;
+        PlayerStamina2.innerText = "Stamina: " + player.stamina;
+        // wait for 1 second and then call shadow_figure_attack
+        if (shadow_figure.health >= 0) {
+            setTimeout(shadow_figure_attack, 1000);
+            }
+    }
+    playerplasma.onclick = function() {
+        playerplasmas();
+        Playerchoice.style.display = "none";
+        enemyhealth.innerText = "Health: " + shadow_figure.health;
+        FightDialogue.innerText = "You have used plasma! -50 energy and 60 damage!";
+        PlayerStamina1.innerText = player.stamina;
+        PlayerStamina2.innerText = "Stamina: " + player.stamina;
+        // wait for 1 second and then call shadow_figure_attack
+        if (shadow_figure.health >= 0) {
+            setTimeout(shadow_figure_attack, 1000);
+            }
+    }
+    playerattack.onclick = function() {
+        playerattacks();
+        Playerchoice.style.display = "none";
+        enemyhealth.innerText = "Health: " + shadow_figure.health;
+        FightDialogue.innerText = "You attacked the " + shadow_figure.name + " for " + player.attack + " damage!";
+        PlayerStamina1.innerText = player.stamina;
+        PlayerStamina2.innerText = "Stamina: " + player.stamina;
+        // wait for 1 second and then call shadow_figure_attack
+        if (shadow_figure.health >= 0) {
+            setTimeout(shadow_figure_attack, 1000);
+            }
+    }
+    playerheal.onclick = function() {
+        playerheals();
+        Playerchoice.style.display = "none";
+        enemyhealth.innerText = "Health: " + shadow_figure.health;
+        FightDialogue.innerText = "You healed yourself for 15 health and 10 stamina!";
+        PlayerStamina1.innerText = player.stamina;
+        PlayerStamina2.innerText = "Stamina: " + player.stamina;
+        // wait for 1 second and then call shadow_figure_attack
+        if (shadow_figure.health >= 0) {
+        setTimeout(shadow_figure_attack, 1000);
+        }
+    }
+    playerdefend.onclick = function() {
+        playerdefends();
+        Playerchoice.style.display = "none";
+        enemyhealth.innerText = "Health: " + shadow_figure.health;
+        FightDialogue.innerText = "You defended yourself!";
+        PlayerStamina1.innerText = player.stamina;
+        PlayerStamina2.innerText = "Stamina: " + player.stamina;
+        // wait for 1 second and then call shadow_figure_attack
+        setTimeout(() => {
+            Playerchoice.style.display = "block";
+        }, 1000);
+    }
+    function playerattacks() {
+        shadow_figure.health -= player.attack;
+        player.stamina -= 10;
+    }
+    function playerdefends() {
+        player.health -= shadow_figure.attack / 5;
+        player.stamina -= 5;
+    }
+    function playerheals() {
+        player.health += 15;
+        player.stamina += 10;
+    }
+    function playerplasmas() {
+        shadow_figure.health -= 60;
+        player.stamina -= 50;
+        playerplasma.InnerText = "";
+    }
+    function playerkicks() {
+        shadow_figure.health -= 10;
+        player.stamina -= 5;
+    }
+    function shadow_figure_attack() {
+    player.health -= shadow_figure.attack;
+    Playerchoice.style.display = "block";
+    FightDialogue.innerText = "The " + shadow_figure.name + " attacked you for " + shadow_figure.attack + " damage!";
+    }
+    function checkplayerdeath() {
+        if(player.health <= 0 || player.stamina <= 0) {
+            FightScene.style.display = "none";
+            Dialogue.style.display = "block";
+            player.health = 0;
+            textdialogue.InnerText = "You have died, This is most likely because you made a bad choice or you lost the fight or you ran out of stamina. Restart if you want to.";
+            choiceA.InnerText = "Restart";
+            choiceA.onclick = function() {
+                PrisonDialogue();
+                player.health = 100;
+                player.stamina = 100;
+            }
+        }
+    }
+    setInterval(checkplayerdeath, 10);
+    setInterval(checkenemydeath, 10);
+    function checkenemydeath() {
+        if(shadow_figure.health <= 0) {
+            FightScene.style.display = "none";
+            Dialogue.style.display = "block";
+            shadow_figure.health = 0;
+        }
+    }
+}
+
+setTimeout(() => {
+    playerhealth2.innerText = "Health: " + player.health;
+    health.innerText = "Health: " + player.health;
+    PlayerStamina1.innerText = "Stamina: " + player.stamina;
+    PlayerStamina2.innerText = "Stamina: " + player.stamina;
+}, 100);
--- src/Scripts/game.js
+++ src/Scripts/game.js
@@ -9,6 +9,7 @@ export var player = {
     maxStamina: 100,
     level: 1,
     attack: 15,
+    Money: 0,
 }
 // calling the HTML elements
 export const textdialogue = document.getElementById('container');
@@ -20,10 +21,12 @@ export const playerattack = document.getElementById('Attack');
 export const playerheal = document.getElementById('Heal');
 export const playerdefend = document.getElementById('Defend');
 export const playerkick = document.getElementById('Kick');
+export const playerhealth2 = document.getElementById('health2');
 export const playerplasma = document.getElementById('Plasma');
 export const Playerchoice = document.getElementById('PlayerFight');
 export const PlayerStamina1 = document.getElementById('Stamina1');
 export const PlayerStamina2 = document.getElementById('Stamina2');
+export const playermoney = document.getElementById('Money');
 export const FightScene = document.getElementById('fightscene');
 export const enemyhealth = document.getElementById('enemyhealth');
 export const FightDialogue = document.getElementById('fightdialogue');
@@ -50,11 +53,15 @@ function Playerstats() {
     playerhealth.innerText = "Health: " + player.health;
     playerlevel.innerText = "Level: " + player.level;
     PlayerStamina1.innerText = "Stamina: " + player.stamina;
+    playermoney.innerText = "Money: " + player.Money;
     if (player.health <= 0) {
         player.health = 0;
         playerhealth.innerText = player.health;
         textdialogue.innerText = "You have died, This is most likely because you made a bad choice or you lost the fight. Restart if you want to.";
         choiceA.innerText = "Restart";
+        choiceB.innerText = "";
+        choiceC.innerText = "";
+        choiceD.innerText = "";
         choiceA.onclick = function() {
             PrisonDialogue();
             player.health = 100;
@@ -114,6 +121,9 @@ function SewersDialogue() {
         textdialogue.innerText = "You keep going until you reach the end of the sewer. You see a small light, it looks like it leads to the outside. You can slip right through the bars and get out, or you can explore the sewer.";
         choiceA.innerText = "Explore the sewer";
         choiceB.innerHTML = "Slip through the bars";
+        choiceB.onclick = function() {
+            outsideWorld();
+        }
         choiceA.onclick = function() {
             textdialogue.innerText = "You walk the opposite direction, then you take a left and find yourself in a 2 paths leading in 2 different directions. Path 1 has a cold air coming from it. The other path has hot air coming from it. You can either go left or right.";
             choiceA.innerText = "Go left";
@@ -121,6 +131,9 @@ function SewersDialogue() {
             choiceA.onclick = function() {
                 textdialogue.innerText = "You go left, its a long walk but you finally reach the end of the sewer. You see a small light, it looks like it leads to the outside. You can slip right through the bars and get out, or you can explore the sewer by going back to the intersection and going right.";
                 choiceA.innerText = "get out";
+                choiceA.onclick = function() {
+                    outsideWorld();
+                }
             }
             choiceB.onclick = function() {
                 textdialogue.innerText = "You go right, a very short walk after you reach a dead end. Suddenly you hear a loud noise. You turn around and see a fire minion. That explains the hot air. You have no other choice but to fight it.";
@@ -167,7 +180,135 @@ function SewersDialogue() {
 function outsideWorld() {
     textdialogue.innerText = "You have reached the outside world. Its pretty out here, you can see the sun shining through the trees.";
     choiceA.innerText = "Continue";
+    choiceB.innerText = "";
+    choiceC.innerText = "";
+    choiceD.innerText = "";
     choiceA.onclick = function() {
-        textdialogue.innerText = "You continue walking, you see people walking around, You smell ";
+        textdialogue.innerText = "You continue walking, you see people walking around, You smell sweet fragrance of cinnamon rolls. Too bad you dont have any money.";
+        choiceA.innerText = "Continue";
+        choiceA.onclick = function() {
+            textdialogue.innerText = "Suddenly, you find a $10 bill on the ground. What should you do, take it or leave it?";
+            choiceA.innerText = "Take it";
+            choiceB.innerText = "Leave it";
+            choiceB.onclick = function() {
+                textdialogue.innerText = "You leave the $10 bill on the ground, you continue walking.";
+                choiceA.innerText = "Continue";
+                choiceB.innerText = "";
+                choiceA.onclick = function() {
+                    outsidebattle();
+                }
+            }
+            choiceA.onclick = function() {
+                textdialogue.innerText = "You take the money and now you have some money to buy some food.";
+                player.Money = player.Money + 10;
+                choiceA.innerText = "Continue";
+                choiceB.innerText = "";
+                choiceA.onclick = function() {
+                    textdialogue.innerText = "You can either use the money to buy some food or you can save it for later.";
+                    choiceA.innerText = "Buy food";
+                    choiceB.innerText = "Save it";
+                    choiceC.innerText = "";
+                    choiceD.innerText = "";
+                    choiceB.onclick = function() {
+                        textdialogue.innerText = "You save the money for later.";
+                        choiceA.innerText = "Continue";
+                        choiceB.innerText = "";
+                        choiceA.onclick = function() {
+                            outsidebattle();
+                        }
+                    }
+                    choiceA.onclick = function() {
+                        textdialogue.innerText = "You have $10 to spend, you can buy a sandwich, a salad, or a cup of coffee.";
+                        choiceA.innerText = "Sandwich: $4";
+                        choiceB.innerText = "Salad: $8";
+                        choiceC.innerText = "Coffee: $10";
+                        choiceA.onclick = function() {
+                            player.Money = player.Money - 4;
+                            textdialogue.innerText = "You buy a sandwich, you are full and you can go outside. You have gained 10 health.";
+                            player.health = player.health + 10;
+                            choiceA.innerText = "Go outside";
+                            choiceB.innerText = "";
+                            choiceC.innerText = "";
+                            choiceD.innerText = "";
+                            choiceA.onclick = function() {
+                                outsidebattle();
+                            }
+                            }
+                            choiceB.onclick = function() {
+                                player.Money = player.Money - 8;
+                                textdialogue.innerText = "You buy a salad, you are full and you can go outside. You have gained 20 stamina.";
+                                player.stamina = player.stamina + 20;
+                                choiceA.innerText = "Go outside";
+                                choiceB.innerText = "";
+                                choiceC.innerText = "";
+                                choiceD.innerText = "";
+                                choiceA.onclick = function() {
+                                    outsidebattle();
+                                }
+                                }
+                                }
+                            choiceC.onclick = function() {
+                                    player.Money = player.Money - 10;
+                                    textdialogue.innerText = "You buy a cup of coffee, you are full and you can go outside. You have gained 10 health and stamina.";
+                                    player.health = player.health + 10;
+                                    player.stamina = player.stamina + 10;
+                                    choiceA.innerText = "Go outside";
+                                    choiceB.innerText = "";
+                                    choiceC.innerText = "";
+                                    choiceD.innerText = "";
+                                    choiceA.onclick = function() {
+                                        outsidebattle();
+                                    }
+                                    }
+                                    }
+                            }
+            }
+        }
     }
+
+function outsidebattle() {
+    textdialogue.innerText = "Now that you are outside, you are full of energy and are ready to fight any enemy you encounter.";
+    choiceA.innerText = "Continue";
+    choiceB.innerText = "";
+    choiceC.innerText = "";
+    choiceD.innerText = "";
+    choiceA.onclick = function() {
+        textdialogue.innerText = "You now started to question why you were in the prison. You remember a solder shouting 'capture him!' right before you fainted.";
+        choiceA.innerText = "Continue";
+        choiceB.innerText = "";
+        choiceC.innerText = "";
+        choiceD.innerText = "";
+        choiceA.onclick = function() {
+            textdialogue.innerText = "Your eyes suddenly saw a black shadow in the distance. You were in a dark alleyway. You were not sure if you were being watched.";
+            choiceA.innerText = "Continue";
+            choiceB.innerText = "";
+            choiceA.onclick = function() {
+                textdialogue.innerText = "The shadow made eye contact with you. The shadow prepares to attack you.";
+                choiceA.innerText = "Fight";
+                choiceB.innerText = "Run";
+                choiceA.onclick = function() {
+                    textdialogue.innerText = "You fight the shadow. You are able to kill it.";
+                    choiceA.innerText = "Continue";
+                    choiceB.innerText = "";
+                    enemy.shadowfigure();
+                    choiceA.onclick = function() {
+                        textdialogue.innerText = "You run away not bothering to look who the shadow was. You exit the alleyway and continue on your way.";
+                        choiceA.innerText = "Continue";
+                        choiceB.innerText = "";
+                        choiceA.onclick = function() {
+                            hotel();
+                        }
+                    }
+                }
+                choiceB.onclick = function() {
+                    textdialogue.innerText = "You run away but you are slow and you are dead.";
+                    player.health = 0;
+                }
+            }
+        }
+    }
+}
+
+function hotel() {
+    textdialogue.innerText = "Its night time. You decide it would be best to stay in the hotel. You are tired and you are hungry. Sadly you dont have enough money to afford the food at the hotel.";
 }
