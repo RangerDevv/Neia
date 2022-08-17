825772d8d9f7ef6230a4982ba300e9fe754f2e9f
Rangerdevv <meerabdul2008@gmail.com>
2022-08-17 16:00:09 -0400

    update player stats

--- pushupdates.sh
+++ pushupdates.sh
@@ -1,5 +1,5 @@
 #bin/bash
 
 git add .
-git commit -m "Basic stuff "
+git commit -m "update player stats "
 git push 
--- src/Scripts/game.js
+++ src/Scripts/game.js
@@ -4,9 +4,16 @@ import { Player } from "./player.js";
 // Variables:
 let Text = document.getElementById("container");
 let Health = document.getElementById("health");
-let Level = document.getElementById("Level");
+let Level = document.getElementById("level");
 let Exp = document.getElementById("Exp");
 let Money = document.getElementById("Money");
 
+// Update the player stats:
+setInterval(function () {
+    Health.innerText = "Health: " + Player.health;
+    Level.innerText ="Level: "+ Player.level;
+    Exp.innerText = "XP: " + Player.exp;
+    Money.innerText = "Money: "+Player.money;
+} ,500);
 
-
+// Functions: Start of the game:
